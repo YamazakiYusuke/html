8d0a3c3e7c333ab51cfc3f30a9e3b85f499a614d
YamazakiYusuke <yusuke38098@gmail.com>
2020-12-22 18:16:02 +0900

    fix translations index

---
+++ 3graduation_app/test/test.rb
@@ -0,0 +1,85 @@
+ja_book = "小田原熱海あたみ間に、軽便鉄道敷設ふせつの工事が始まったのは、良平りょうへいの八つの年だった。良平は毎日村外はずれへ、その工事を見物に行った。工事を――といったところが、唯ただトロッコで土を運搬する――それが面白さに見に行ったのである。
+  　トロッコの上には土工が二人、土を積んだ後うしろに佇たたずんでいる。トロッコは山を下くだるのだから、人手を借りずに走って来る。煽あおるように車台が動いたり、土工の袢天はんてんの裾すそがひらついたり、細い線路がしなったり――良平はそんなけしきを眺ながめながら、土工になりたいと思う事がある。せめては一度でも土工と一しょに、トロッコへ乗りたいと思う事もある。トロッコは村外れの平地へ来ると、自然と其処そこに止まってしまう。と同時に土工たちは、身軽にトロッコを飛び降りるが早いか、その線路の終点へ車の土をぶちまける。それから今度はトロッコを押し押し、もと来た山の方へ登り始める。良平はその時乗れないまでも、押す事さえ出来たらと思うのである。
+  　或ある夕方、――それは二月の初旬だった。良平は二つ下の弟や、弟と同じ年の隣の子供と、トロッコの置いてある村外れへ行った。トロッコは泥だらけになったまま、薄明るい中に並んでいる。が、その外ほかは何処どこを見ても、土工たちの姿は見えなかった。三人の子供は恐る恐る、一番端はしにあるトロッコを押した。トロッコは三人の力が揃そろうと、突然ごろりと車輪をまわした。良平はこの音にひやりとした。しかし二度目の車輪の音は、もう彼を驚かさなかった。ごろり、ごろり、――トロッコはそう云う音と共に、三人の手に押されながら、そろそろ線路を登って行った。
+  　その内にかれこれ十間けん程来ると、線路の勾配こうばいが急になり出した。トロッコも三人の力では、いくら押しても動かなくなった。どうかすれば車と一しょに、押し戻されそうにもなる事がある。良平はもう好よいと思ったから、年下の二人に合図をした。
+  「さあ、乗ろう！」
+  　彼等は一度に手をはなすと、トロッコの上へ飛び乗った。トロッコは最初徐おもむろに、それから見る見る勢いきおいよく、一息に線路を下くだり出した。その途端につき当りの風景は、忽たちまち両側へ分かれるように、ずんずん目の前へ展開して来る。顔に当る薄暮はくぼの風、足の下に躍おどるトロッコの動揺、――良平は殆ほとんど有頂天うちょうてんになった。
+  　しかしトロッコは二三分の後のち、もうもとの終点に止まっていた。
+  「さあ、もう一度押すじゃあ」
+  　良平は年下の二人と一しょに、又トロッコを押し上げにかかった。が、まだ車輪も動かない内に、突然彼等の後うしろには、誰かの足音が聞え出した。のみならずそれは聞え出したと思うと、急にこう云う怒鳴り声に変った。
+  「この野郎！　誰に断ことわってトロに触さわった？」
+  　其処には古い印袢天しるしばんてんに、季節外れの麦藁帽むぎわらぼうをかぶった、背の高い土工が佇んでいる。――そう云う姿が目にはいった時、良平は年下の二人と一しょに、もう五六間逃げ出していた。――それぎり良平は使の帰りに、人気のない工事場のトロッコを見ても、二度と乗って見ようと思った事はない。唯その時の土工の姿は、今でも良平の頭の何処かに、はっきりした記憶を残している。薄明りの中に仄ほのめいた、小さい黄色の麦藁帽、――しかしその記憶さえも、年毎としごとに色彩は薄れるらしい。
+  　その後のち十日余りたってから、良平は又たった一人、午ひる過ぎの工事場に佇みながら、トロッコの来るのを眺めていた。すると土を積んだトロッコの外ほかに、枕木まくらぎを積んだトロッコが一輛りょう、これは本線になる筈はずの、太い線路を登って来た。このトロッコを押しているのは、二人とも若い男だった。良平は彼等を見た時から、何だか親しみ易やすいような気がした。「この人たちならば叱しかられない」――彼はそう思いながら、トロッコの側そばへ駈かけて行った。
+  「おじさん。押してやろうか？」
+  　その中の一人、――縞しまのシャツを着ている男は、俯向うつむきにトロッコを押したまま、思った通り快い返事をした。
+  「おお、押してくよう」
+  　良平は二人の間にはいると、力一杯押し始めた。
+  「われは中中なかなか力があるな」
+  　他たの一人、――耳に巻煙草まきたばこを挟はさんだ男も、こう良平を褒ほめてくれた。
+  　その内に線路の勾配は、だんだん楽になり始めた。「もう押さなくとも好よい」――良平は今にも云われるかと内心気がかりでならなかった。が、若い二人の土工は、前よりも腰を起したぎり、黙黙と車を押し続けていた。良平はとうとうこらえ切れずに、怯おず怯おずこんな事を尋ねて見た。
+  「何時いつまでも押していて好いい？」
+  「好いとも」
+  　二人は同時に返事をした。良平は「優しい人たちだ」と思った。
+  　五六町余り押し続けたら、線路はもう一度急勾配になった。其処には両側の蜜柑畑みかんばたけに、黄色い実がいくつも日を受けている。
+  「登り路みちの方が好い、何時いつまでも押させてくれるから」――良平はそんな事を考えながら、全身でトロッコを押すようにした。
+  　蜜柑畑の間を登りつめると、急に線路は下くだりになった。縞のシャツを着ている男は、良平に「やい、乗れ」と云った。良平は直すぐに飛び乗った。トロッコは三人が乗り移ると同時に、蜜柑畑の※(「均のつくり」、第3水準1-14-75)においを煽あおりながら、ひた辷すべりに線路を走り出した。「押すよりも乗る方がずっと好い」――良平は羽織に風を孕はらませながら、当り前の事を考えた。「行きに押す所が多ければ、帰りに又乗る所が多い」――そうもまた考えたりした。
+  　竹藪たけやぶのある所へ来ると、トロッコは静かに走るのを止やめた。三人は又前のように、重いトロッコを押し始めた。竹藪は何時か雑木林になった。爪先つまさき上りの所所ところどころには、赤錆あかさびの線路も見えない程、落葉のたまっている場所もあった。その路をやっと登り切ったら、今度は高い崖がけの向うに、広広と薄ら寒い海が開けた。と同時に良平の頭には、余り遠く来過ぎた事が、急にはっきりと感じられた。
+  　三人は又トロッコへ乗った。車は海を右にしながら、雑木の枝の下を走って行った。しかし良平はさっきのように、面白い気もちにはなれなかった。「もう帰ってくれれば好いい」――彼はそうも念じて見た。が、行く所まで行きつかなければ、トロッコも彼等も帰れない事は、勿論もちろん彼にもわかり切っていた。
+  　その次に車の止まったのは、切崩きりくずした山を背負っている、藁屋根の茶店の前だった。二人の土工はその店へはいると、乳呑児ちのみごをおぶった上かみさんを相手に、悠悠ゆうゆうと茶などを飲み始めた。良平は独ひとりいらいらしながら、トロッコのまわりをまわって見た。トロッコには頑丈がんじょうな車台の板に、跳はねかえった泥が乾かわいていた。
+  　少時しばらくの後のち茶店を出て来しなに、巻煙草を耳に挟はさんだ男は、（その時はもう挟んでいなかったが）トロッコの側にいる良平に新聞紙に包んだ駄菓子をくれた。良平は冷淡に「難有ありがとう」と云った。が、直すぐに冷淡にしては、相手にすまないと思い直した。彼はその冷淡さを取り繕うように、包み菓子の一つを口へ入れた。菓子には新聞紙にあったらしい、石油の※(「均のつくり」、第3水準1-14-75)がしみついていた。
+  　三人はトロッコを押しながら緩ゆるい傾斜を登って行った。良平は車に手をかけていても、心は外ほかの事を考えていた。
+  　その坂を向うへ下おり切ると、又同じような茶店があった。土工たちがその中へはいった後あと、良平はトロッコに腰をかけながら、帰る事ばかり気にしていた。茶店の前には花のさいた梅に、西日の光が消えかかっている。「もう日が暮れる」――彼はそう考えると、ぼんやり腰かけてもいられなかった。トロッコの車輪を蹴けって見たり、一人では動かないのを承知しながらうんうんそれを押して見たり、――そんな事に気もちを紛らせていた。
+  　ところが土工たちは出て来ると、車の上の枕木まくらぎに手をかけながら、無造作むぞうさに彼にこう云った。
+  「われはもう帰んな。おれたちは今日は向う泊りだから」
+  「あんまり帰りが遅くなるとわれの家うちでも心配するずら」
+  　良平は一瞬間呆気あっけにとられた。もうかれこれ暗くなる事、去年の暮母と岩村まで来たが、今日の途みちはその三四倍ある事、それを今からたった一人、歩いて帰らなければならない事、――そう云う事が一時にわかったのである。良平は殆ほとんど泣きそうになった。が、泣いても仕方がないと思った。泣いている場合ではないとも思った。彼は若い二人の土工に、取って附けたような御時宜おじぎをすると、どんどん線路伝いに走り出した。
+  　良平は少時しばらく無我夢中に線路の側を走り続けた。その内に懐ふところの菓子包みが、邪魔になる事に気がついたから、それを路側みちばたへ抛ほり出す次手ついでに、板草履いたぞうりも其処へ脱ぎ捨ててしまった。すると薄い足袋たびの裏へじかに小石が食いこんだが、足だけは遙はるかに軽くなった。彼は左に海を感じながら、急な坂路さかみちを駈かけ登った。時時涙がこみ上げて来ると、自然に顔が歪ゆがんで来る。――それは無理に我慢しても、鼻だけは絶えずくうくう鳴った。
+  　竹藪の側を駈け抜けると、夕焼けのした日金山ひがねやまの空も、もう火照ほてりが消えかかっていた。良平は、愈いよいよ気が気でなかった。往ゆきと返かえりと変るせいか、景色の違うのも不安だった。すると今度は着物までも、汗の濡ぬれ通ったのが気になったから、やはり必死に駈け続けたなり、羽織を路側みちばたへ脱いで捨てた。
+  　蜜柑畑へ来る頃には、あたりは暗くなる一方だった。「命さえ助かれば――」良平はそう思いながら、辷すべってもつまずいても走って行った。
+  　やっと遠い夕闇ゆうやみの中に、村外れの工事場が見えた時、良平は一思いに泣きたくなった。しかしその時もべそはかいたが、とうとう泣かずに駈け続けた。
+  　彼の村へはいって見ると、もう両側の家家には、電燈の光がさし合っていた。良平はその電燈の光に、頭から汗の湯気ゆげの立つのが、彼自身にもはっきりわかった。井戸端に水を汲くんでいる女衆おんなしゅうや、畑から帰って来る男衆おとこしゅうは、良平が喘あえぎ喘ぎ走るのを見ては、「おいどうしたね？」などと声をかけた。が、彼は無言のまま、雑貨屋だの床屋だの、明るい家の前を走り過ぎた。
+  　彼の家うちの門口かどぐちへ駈けこんだ時、良平はとうとう大声に、わっと泣き出さずにはいられなかった。その泣き声は彼の周囲まわりへ、一時に父や母を集まらせた。殊ことに母は何とか云いながら、良平の体を抱かかえるようにした。が、良平は手足をもがきながら、啜すすり上げ啜り上げ泣き続けた。その声が余り激しかったせいか、近所の女衆も三四人、薄暗い門口へ集って来た。父母は勿論その人たちは、口口に彼の泣く訣わけを尋ねた。しかし彼は何と云われても泣き立てるより外に仕方がなかった。あの遠い路を駈け通して来た、今までの心細さをふり返ると、いくら大声に泣き続けても、足りない気もちに迫られながら、…………
+  　良平は二十六の年、妻子さいしと一しょに東京へ出て来た。今では或雑誌社の二階に、校正の朱筆しゅふでを握っている。が、彼はどうかすると、全然何の理由もないのに、その時の彼を思い出す事がある。全然何の理由もないのに？――塵労じんろうに疲れた彼の前には今でもやはりその時のように、薄暗い藪や坂のある路が、細細と一すじ断続している。"
+
+en_book = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, “and what is the use of a book,” thought Alice “without pictures or conversations?”
+
+So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a White Rabbit with pink eyes ran close by her.
+
+There was nothing so very remarkable in that; nor did Alice think it so very much out of the way to hear the Rabbit say to itself, “Oh dear! Oh dear! I shall be late!” (when she thought it over afterwards, it occurred to her that she ought to have wondered at this, but at the time it all seemed quite natural); but when the Rabbit actually took a watch out of its waistcoat-pocket, and looked at it, and then hurried on, Alice started to her feet, for it flashed across her mind that she had never before seen a rabbit with either a waistcoat-pocket, or a watch to take out of it, and burning with curiosity, she ran across the field after it, and fortunately was just in time to see it pop down a large rabbit-hole under the hedge.
+
+In another moment down went Alice after it, never once considering how in the world she was to get out again.
+
+The rabbit-hole went straight on like a tunnel for some way, and then dipped suddenly down, so suddenly that Alice had not a moment to think about stopping herself before she found herself falling down a very deep well.
+
+Either the well was very deep, or she fell very slowly, for she had plenty of time as she went down to look about her and to wonder what was going to happen next. First, she tried to look down and make out what she was coming to, but it was too dark to see anything; then she looked at the sides of the well, and noticed that they were filled with cupboards and book-shelves; here and there she saw maps and pictures hung upon pegs. She took down a jar from one of the shelves as she passed; it was labelled “ORANGE MARMALADE”, but to her great disappointment it was empty: she did not like to drop the jar for fear of killing somebody underneath, so managed to put it into one of the cupboards as she fell past it.
+
+“Well!” thought Alice to herself, “after such a fall as this, I shall think nothing of tumbling down stairs! How brave they’ll all think me at home! Why, I wouldn’t say anything about it, even if I fell off the top of the house!” (Which was very likely true.)
+
+Down, down, down. Would the fall never come to an end? “I wonder how many miles I’ve fallen by this time?” she said aloud. “I must be getting somewhere near the centre of the earth. Let me see: that would be four thousand miles down, I think—” (for, you see, Alice had learnt several things of this sort in her lessons in the schoolroom, and though this was not a very good opportunity for showing off her knowledge, as there was no one to listen to her, still it was good practice to say it over) “—yes, that’s about the right distance—but then I wonder what Latitude or Longitude I’ve got to?” (Alice had no idea what Latitude was, or Longitude either, but thought they were nice grand words to say.)
+
+Presently she began again. “I wonder if I shall fall right through the earth! How funny it’ll seem to come out among the people that walk with their heads downward! The Antipathies, I think—” (she was rather glad there was no one listening, this time, as it didn’t sound at all the right word) “—but I shall have to ask them what the name of the country is, you know. Please, Ma’am, is this New Zealand or Australia?” (and she tried to curtsey as she spoke—fancy curtseying as you’re falling through the air! Do you think you could manage it?) “And what an ignorant little girl she’ll think me for asking! No, it’ll never do to ask: perhaps I shall see it written up somewhere.”
+
+Down, down, down. There was nothing else to do, so Alice soon began talking again. “Dinah’ll miss me very much to-night, I should think!” (Dinah was the cat.) “I hope they’ll remember her saucer of milk at tea-time. Dinah my dear! I wish you were down here with me! There are no mice in the air, I’m afraid, but you might catch a bat, and that’s very like a mouse, you know. But do cats eat bats, I wonder?” And here Alice began to get rather sleepy, and went on saying to herself, in a dreamy sort of way, “Do cats eat bats? Do cats eat bats?” and sometimes, “Do bats eat cats?” for, you see, as she couldn’t answer either question, it didn’t much matter which way she put it. She felt that she was dozing off, and had just begun to dream that she was walking hand in hand with Dinah, and saying to her very earnestly, “Now, Dinah, tell me the truth: did you ever eat a bat?” when suddenly, thump! thump! down she came upon a heap of sticks and dry leaves, and the fall was over.
+
+Alice was not a bit hurt, and she jumped up on to her feet in a moment: she looked up, but it was all dark overhead; before her was another long passage, and the White Rabbit was still in sight, hurrying down it. There was not a moment to be lost: away went Alice like the wind, and was just in time to hear it say, as it turned a corner, “Oh my ears and whiskers, how late it’s getting!” She was close behind it when she turned the corner, but the Rabbit was no longer to be seen: she found herself in a long, low hall, which was lit up by a row of lamps hanging from the roof.
+
+There were doors all round the hall, but they were all locked; and when Alice had been all the way down one side and up the other, trying every door, she walked sadly down the middle, wondering how she was ever to get out again.
+
+Suddenly she came upon a little three-legged table, all made of solid glass; there was nothing on it except a tiny golden key, and Alice’s first thought was that it might belong to one of the doors of the hall; but, alas! either the locks were too large, or the key was too small, but at any rate it would not open any of them. However, on the second time round, she came upon a low curtain she had not noticed before, and behind it was a little door about fifteen inches high: she tried the little golden key in the lock, and to her great delight it fitted!
+
+Alice opened the door and found that it led into a small passage, not much larger than a rat-hole: she knelt down and looked along the passage into the loveliest garden you ever saw. How she longed to get out of that dark hall, and wander about among those beds of bright flowers and those cool fountains, but she could not even get her head through the doorway; “and even if my head would go through,” thought poor Alice, “it would be of very little use without my shoulders. Oh, how I wish I could shut up like a telescope! I think I could, if I only knew how to begin.” For, you see, so many out-of-the-way things had happened lately, that Alice had begun to think that very few things indeed were really impossible.
+
+There seemed to be no use in waiting by the little door, so she went back to the table, half hoping she might find another key on it, or at any rate a book of rules for shutting people up like telescopes: this time she found a little bottle on it, (“which certainly was not here before,” said Alice,) and round the neck of the bottle was a paper label, with the words “DRINK ME,” beautifully printed on it in large letters.
+
+It was all very well to say “Drink me,” but the wise little Alice was not going to do that in a hurry. “No, I’ll look first,” she said, “and see whether it’s marked ‘poison’ or not”; for she had read several nice little histories about children who had got burnt, and eaten up by wild beasts and other unpleasant things, all because they would not remember the simple rules their friends had taught them: such as, that a red-hot poker will burn you if you hold it too long; and that if you cut your finger very deeply with a knife, it usually bleeds; and she had never forgotten that, if you drink much from a bottle marked “poison,” it is almost certain to disagree with you, sooner or later.
+
+However, this bottle was not marked “poison,” so Alice ventured to taste it, and finding it very nice, (it had, in fact, a sort of mixed flavour of cherry-tart, custard, pine-apple, roast turkey, toffee, and hot buttered toast,) she very soon finished it off."
+
+ru_book = "В колыбельной песне, сказке, прибаутке, былине художественное слово является нам раньше всего, о нем и чуть ли не самая первая память. Есть и память, заключенная в нем самом, причем память самая долгая и самая всеобщая. Как всегда, хорошая память указует вперед, образует дорогу. Надо знать и дорогу, и направление. «Дорогато есть, я стою на твердой полосе; да что толку?» Необычайно меткое это усмотрение Пушкина в «Капитанской дочке». Человек, который у него так говорил, вроде бы знал народ, был сам совершенно народен; Петруше он со значением рассказал и известную сказку о разнице между горячей живой кровью своих жертв и чьимто питанием мертвечиной (орел и ворон). Однако вопрос об истинном направлении он лично оставил открытым, и для народа не меньше, чем для себя.
+Содержанием, состоянием, «безумной прихотью» отдельного человека не исчерпывается достояние всего народа.
+Отдельный человек может вообще не знать поговорок и сказок, а былин не слыхать и подавно. Ктото не может припомнить по своему детству как раз самых драгоценных слов. А народу в целом именно полнота ранней памяти и свойственна – как по «жанрам», так и по содержательному существу.
+Народ помнит себя вовсе не только со дня, когда ктото умный привел его за руку в довольно среднюю школу и сказал: теперь, маленький, поучись. Это не значит, что настоящего детства у народа не было. Один классический мыслитель именно о народах древности и заметил: есть буйные, невоспитанные дети, и есть старчески умные дети. Нормальными детьми были греки, добавил классик: Прометей, Эдип, Дедал, Геракл – там везде дерзание, везде бодрость и здоровье смолоду. Он не разъяснил, кто относится к первым двум разновидностям, и науке стоит заняться этим особо. Но детство как ценность признается им не без оснований.
+К этой детской, но коренной, к этой дописьменной, но ясной и твердой памяти народов тянется здоровая память отдельных людей, и это для зрелости только полезно. Иногда без этого настоящая зрелость и невозможна. Это видно по самым умным людям России. «Ах, умолчу ль о мамушке моей», – пишет Пушкин и тут же переходит к сказкам «мамушки» про лихих полканов и добрыней, о чудесах и подвигах Бовы. Поздний пушкинский «Гусар» сталкивается с чудесами малоблагообразными, но именно лихость доброго сказочного склада ему и помогает уйти от «мерзостной игры» – даже, помнится, сорвать её. Пушкин к тому времени уже давно созрел; он получил немало именно от няни – вдобавок к царственному Лицею; а искушенным литераторам, не знавшим этого, он советовал учиться коечему у просвирней, не обольщаясь ни лицеем, ни университетом. Слушаю сказки няни – и восполняю тем недостаток своего проклятого образования… Как честно, как смело сказано! Ах, нянины сказки, что за прелесть они: каждая народная сказка есть поэма, по Пушкину. Без этой убежденности великого художника не родились бы ни «Руслан и Людмила», ни «Царь Салтан», ни даже «Капитанская дочка» – то есть именно прямые поэмы, желающие сравняться с народною сказкой; так музыка Глинки и Чайковского или «Снегурочки» равнялась на русскую сказку и русский народный напев, и только потому преуспела как всемирная ценность. (Да так и во всех здоровых обществах: что такое «Лесной царь» величественного Гёте?)"
+
+en_book.chomp
+en_book.scan(.*?[\.?!。？！])
